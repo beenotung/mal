f65338ceccbcdcd8be023e7cfb3cdfc40cdcdad3
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-06-17 16:31:04 +0800

    simplify context argument of castRational

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -84,19 +84,19 @@ function add_two(left: AST, right: AST): AST {
     return left + right
   }
   if (typeof left === 'number') {
-    right = castRational(right, { when: 'add', message: 'expect Num' })
+    right = castRational(right, { when: 'add' })
     let down = right.down
     let up = left * down + right.up
     return rational(up, down)
   }
   if (typeof right === 'number') {
-    left = castRational(left, { when: 'add', message: 'expect Num' })
+    left = castRational(left, { when: 'add' })
     let down = left.down
     let up = left.up + right * down
     return rational(up, down)
   }
-  left = castRational(left, { when: 'add', message: 'expect Num' })
-  right = castRational(right, { when: 'add', message: 'expect Num' })
+  left = castRational(left, { when: 'add' })
+  right = castRational(right, { when: 'add' })
   let down = lcm(left.down, right.down)
   let up = (down / left.down) * left.up + (down / right.down) * right.up
   return rational(up, down)
@@ -111,7 +111,7 @@ function minus(args: AST[]): AST {
     if (typeof left === 'number') {
       return -left
     }
-    left = castRational(left, { when: 'minus', message: 'expect Num' })
+    left = castRational(left, { when: 'minus' })
     return rational(-left.up, left.down)
   }
   for (let i = 1; i < args.length; i++) {
@@ -126,19 +126,19 @@ function minus_two(left: AST, right: AST): AST {
     return left - right
   }
   if (typeof left === 'number') {
-    right = castRational(right, { when: 'minus', message: 'expect Num' })
+    right = castRational(right, { when: 'minus' })
     let down = right.down
     let up = left * down - right.up
     return rational(up, down)
   }
   if (typeof right === 'number') {
-    left = castRational(left, { when: 'minus', message: 'expect Num' })
+    left = castRational(left, { when: 'minus' })
     let down = left.down
     let up = left.up - right * down
     return rational(up, down)
   }
-  left = castRational(left, { when: 'minus', message: 'expect Num' })
-  right = castRational(right, { when: 'minus', message: 'expect Num' })
+  left = castRational(left, { when: 'minus' })
+  right = castRational(right, { when: 'minus' })
   let down = lcm(left.down, right.down)
   let up = (down / left.down) * left.up - (down / right.down) * right.up
   return rational(up, down)
@@ -162,19 +162,19 @@ function multiply_two(left: AST, right: AST): AST {
     return left * right
   }
   if (typeof left === 'number') {
-    right = castRational(right, { when: 'multiply', message: 'expect Num' })
+    right = castRational(right, { when: 'multiply' })
     let up = left * right.up
     let down = right.down
     return rational(up, down)
   }
   if (typeof right === 'number') {
-    left = castRational(left, { when: 'multiply', message: 'expect Num' })
+    left = castRational(left, { when: 'multiply' })
     let up = left.up * right
     let down = left.down
     return rational(up, down)
   }
-  left = castRational(left, { when: 'multiply', message: 'expect Num' })
-  right = castRational(right, { when: 'multiply', message: 'expect Num' })
+  left = castRational(left, { when: 'multiply' })
+  right = castRational(right, { when: 'multiply' })
   let up = left.up * right.up
   let down = left.down * right.down
   return rational(up, down)
@@ -199,7 +199,7 @@ function one_divide(ast: AST): AST {
   if (typeof ast === 'number') {
     return rational(1, ast)
   }
-  ast = castRational(ast, { when: 'divide', message: 'expect Num' })
+  ast = castRational(ast, { when: 'divide' })
   return rational(ast.down, ast.up)
 }
 
@@ -211,16 +211,16 @@ function divide_two(left: AST, right: AST): AST {
   if (typeof right === 'number') {
     right = rational(1, right)
   } else {
-    right = castRational(right, { when: 'divide', message: 'expect Num' })
+    right = castRational(right, { when: 'divide' })
     right = rational(right.down, right.up)
   }
   return multiply_two(left, right)
 }
 
-function castRational(ast: AST, context: ContextType): Rational {
+function castRational(ast: AST, context: { when: string }): Rational {
   if (typeof ast == 'object' && !Array.isArray(ast) && ast.type === 'rational')
     return ast
-  throw new EvaluationError({ ...context, ast })
+  throw new EvaluationError({ when: context.when, message: 'expect Num', ast })
 }
 
 type ContextType<T extends object = {}> = T & {
